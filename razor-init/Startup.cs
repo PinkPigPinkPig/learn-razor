781c62f08ee1f978e4143b111f97d50c7f0ec25e
PinkPigPinkPig <vuquytuan2706@gmail.com>
2023-07-19 17:34:11 +0700

    razor

--- razor-init/Startup.cs
+++ razor-init/Startup.cs
@@ -16,6 +16,10 @@ namespace razor_init
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages().AddRazorPagesOptions(options => {
+                options.RootDirectory = "/Pages";
+                options.Conventions.AddPageRoute("/FirstPage", "/trang-dau-tien");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +34,7 @@ namespace razor_init
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
